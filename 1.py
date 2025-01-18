d2d22b72384636e421972da7d8a72fd03c9ded54
genn125 <genn125@gmail.com>
2025-01-18 21:57:35 +0300

    ghbdtn

--- 1.py
+++ 1.py
@@ -9,14 +9,13 @@ class Product:
         self.weight=weight          # общий вес товара (дробное число)
         self.category=category           # категория товара (строка).
 
-
     def __str__ (self):
         return f'{self.name}, {self.weight}, {self.category}'
 
-class  Shop():
-    def __init__(self):
-       self.__file_name = 'products.txt'
+class  Shop:
 
+    # def __init__(self):
+    __file_name = 'products.txt'
 
     def get_products(self):
         self.__file_name=open('products.txt','r')
@@ -42,7 +41,6 @@ p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
 
 print(f'просто вывод в консоль:, {p2}\n')  # __str__
-
 s1.add( p1, p2, p3)
 print(s1.get_products())
 
--- 2.py
+++ 2.py
@@ -1,5 +1,3 @@
-# Домашнее задание по теме "Режимы открытия файлов"
-
 class Product:
     def __init__(self, name: str, weight: float, category: str):
         self.name = name
@@ -15,21 +13,24 @@ class Shop:
     def __init__(self):
         self.__file_name = open('products.txt', 'a')
         self.__file_name.close()
-
+    # Метод считывает всю информацию из файла __file_name,
+    # закрывает его и возвращает единую строку со всеми товарами
+    # из файла __file_name
     def get_products(self):
-
+        # обращение к чтению файла products.txt (self.__file_name)
         get_file = open('products.txt', 'r')
         name_prod = get_file.read()
         get_file.close()
-
+        # возвращение работы метода self.get_products()
         return name_prod
-
+    # метод принимает неограниченное количество объектов класса Product и
+    # добавляет отсутствующие в файл products.txt (self.__file_name)
     def add(self, *products):
-
+        # цикл перебора наименований product в products
         for product in products:
-
+            # условие проверки когда строки product нет в списке products.txt
             if str(product) not in self.get_products():
-
+                # добавление отсутствующего product в файл products.txt
                 file = open('products.txt', 'a+')
                 file.write(f'{str(product)}\n')
                 file.close()
