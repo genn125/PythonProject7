8c109db92b39da6fd7bef838f4ba1dbb15af26f5
genn125 <genn125@gmail.com>
2025-01-21 21:03:28 +0300

    Позиционирование в файле

--- 2.py
+++ 2.py
@@ -1,14 +1,17 @@
-def custom_write(file_name, string):
-    n = 0
-    elem = {}
-    for i in info:
-        file = open(file_name, 'a', encoding='utf-8')
-        tell = (file.tell())
-        n += 1
-        file.write(f'{i}\n')
-        file.close()
-        elem.update({(n, tell):i})
-    return elem
+# Задача "Записать и запомнить":
+from pprint import pprint
+# Создаём функцию custom_write (file_name, strings)
+# которая принимает аргументы file_name - название файла для записи,
+# strings - список строк для записи.
+def custom_write(file_name, strings):
+    strings_positions = {}
+    with open(file_name, 'w', encoding='utf-8') as file:
+        for i, string in enumerate(strings, start=1):
+# Получение номера байта начала строки используем метод tell() перед записью
+            position = file.tell()
+            file.write(string + '\n')
+            strings_positions[(i, position)] = string
+    return strings_positions
 
 info = [
     'Text for tell.',
