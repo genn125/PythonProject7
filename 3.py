45ca06e09f06d436ef6ce3d27d7cd6d7bbb4b03e
genn125 <genn125@gmail.com>
2025-01-21 11:08:09 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,12 +1,1 @@
-def add(self, *products):
-    # цикл перебора наименований product в products
-    for product in products:
-        # условие проверки когда строки product нет в списке products.txt
-        if str(product) not in self.get_products():
-            # добавление отсутствующего product в файл products.txt
-            file = open('products.txt', 'a')
-            file.write(f'{str(product)}\n')
-            file.close()
-        # когда запись product есть в файле products.txt
-        else:
-            print(f'Продукт {product} уже есть в магазине')
+
