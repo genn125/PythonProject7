30c6357d55e254b7bfe08afc86733e30f3340c7f
genn125 <genn125@gmail.com>
2025-01-22 18:03:27 +0300

    Форматирование строк

--- 7.4.py
+++ 7.4.py
@@ -7,8 +7,8 @@ team2 = '<Волшебники данных>'
 team1_num = 5  # количество участников 1 команды
 team2_num = 6 # количество участников 2 команды
 
-score_1 = 40
-score_2 = 42
+score_1 = 40 # количество задач, решённых командой 1
+score_2 = 42 # количество задач, решённых командой 2
 
 team1_time = 1552.512 # время за которое команда 1 решила задачи
 team2_time = 2153.31451   # время за которое команда 2 решила задачи
@@ -16,19 +16,24 @@ team2_time = 2153.31451   # время за которое команда 2 ре
 # Использование %:
 print('1 - В команде %s участников %s!' % (team1, team1_num))
 print('2 - Итого сегодня в командах участников: %s и %s!' % (team1_num, team2_num))
+
 # Использование format():
 print('3 - Команда {} решила задач: {}!'.format(team2, score_2))
-print('4 - {} решили задачи за {} c!'.format(team2, team2_time))
+print('4 - {} решили задачи за {:.2f} секунды!'.format(team2, team2_time))
+
 # Использование f-строк:
-# print(f'5 - Команды решили {score_1} и {score_2} задач')
+tasks_total = score_1 + score_2
+time_avg=(team1_time + team2_time) / tasks_total # среднее время решения
 
-def challenge_result(tasks_total, time_avg):
-    print(f'5 - Команды решили {score_1} и {score_2} задач')
-    if score_1 > score_2 or score_1 == score_2 and team1_time > team2_time:
-    # if score_1 > score_2:
-        print(f'6 - Результат битвы: победа команды {team1}!')
-    else:
-        print(f'7 - Результат битвы: победа команды {team2}!')
-    print(f'Сегодня было решено {tasks_total} задач, в среднем по {time_avg} секунды на задачу')
+print(f'5 - Команды решили {score_1} и {score_2} задач')
+if (team1_time //team1_num) < (team2_time //team2_num):
+    print(f'6 - Результат битвы: победа команды {team1}! Команда {team1} затратила {team1_time // team1_num} сек на задачу.')
+elif (team1_time //team1_num) > (team2_time //team2_num):
+    print(f'7 - Результат битвы: победа команды {team2}! Команда {team2} затратила {team2_time // team2_num} сек на задачу.')
+else:
+    print(f'Ничья')
 
-challenge_result(82,45.2)
+print(f'8 - Сегодня было решено {tasks_total} задач, в среднем по {time_avg:.1f} секунды на задачу!')
+
+# print(f'Команда {team1} затратила {team1_time // team1_num} сек на задачу')
+# print(f'Команда {team2} затратила {team2_time // team2_num} сек на задачу')
