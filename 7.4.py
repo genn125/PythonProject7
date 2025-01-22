4939ef0aa4c0b022a01ba4bc715a173204fdde28
genn125 <genn125@gmail.com>
2025-01-22 18:31:28 +0300

    Форматирование строк

--- 7.4.py
+++ 7.4.py
@@ -23,17 +23,19 @@ print('4 - {} решили задачи за {:.2f} секунды!'.format(team
 
 # Использование f-строк:
 tasks_total = score_1 + score_2
-time_avg=(team1_time + team2_time) / tasks_total # среднее время решения
+time_avg=(team1_time + team2_time) / tasks_total # среднее время решения задачи
 
-print(f'5 - Команды решили {score_1} и {score_2} задач')
-if team1_time //team1_num < team2_time //team2_num:
-    print(f'6 - Результат битвы: победа команды {team1}! Команда {team1} затратила {team1_time // team1_num} сек на задачу.')
-elif team1_time //team1_num > team2_time //team2_num:
-    print(f'7 - Результат битвы: победа команды {team2}! Команда {team2} затратила {team2_time // team2_num} сек на задачу.')
+print(f'5 - Команды решили {score_1} и {score_2} задач.')
+# if score_1 > score_2 or score_1 == score_2 and team1_time < team2_time:
+if team1_time // (team1_num * score_1) < team2_time // (team2_num * score_2):
+    print(f'6 - Результат битвы: победа команды {team1}!')
+# elif score_1 < score_2 or score_1 == score_2 and team1_time > team2_time:
+elif team1_time // (team1_num * score_1) > team2_time // (team2_num * score_2):
+    print(f'7 - Результат битвы: победа команды {team2}!')
 else:
-    print(f'н')
+    print(f'Ничья')
 
 print(f'8 - Сегодня было решено {tasks_total} задач, в среднем по {time_avg:.1f} секунды на задачу!')
 
-print(f'Команда {team1} затратила {team1_time // team1_num} сек на задачу')
-print(f'Команда {team2} затратила {team2_time // team2_num} сек на задачу')
+print(f'\nКаждый из {team1} затратил {team1_time // (team1_num * score_1)} сек. на задачу.')
+print(f'Каждый из {team2} затратил {team2_time // (team2_num * score_2)} сек. на задачу.')
